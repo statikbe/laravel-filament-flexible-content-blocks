1ce7e07e9923a138ded42b99f0c1111bf77b2c12
Stijn Elskens <stijn@statik.be>
2023-02-10 14:25:56 +0100

    Update text image and image template

--- resources/views/content-blocks/image.blade.php
+++ resources/views/content-blocks/image.blade.php
@@ -1,8 +1,23 @@
-<div class="max-w-4xl">
-    {{$getImageMedia(['alt' => 'hewre', 'class'=> 'booyakasha'])}}
+@php
+    $imagePositionClass = $imagePosition === 'right' ? 'md:flex-row-reverse' : '';
+    // if ($getImageWidthClass() === 'w-3/4') {
+    //     $imageWidthClass = 'md:w-3/4';
+    //  } else if ($getImageWidthClass() === 'w-1/2') {
+    //     $imageWidthClass = 'md:w-1/2';
+    //  } else if ($getImageWidthClass() === 'w-1/3') {
+    //     $imageWidthClass = 'md:w-1/3';
+    //  } else { // w-full
+    //     $imageWidthClass = '';
+    // }
+@endphp
 
-    <span>&checkmark; {{$imageTitle}}</span>
-    <span>&copy; {{$imageCopyright}}</span>
-    <span>{{$imagePosition}}</span>
-    <span>{{$getImageWidthClass()}}</span>
+<div class="py-20 section section--default">
+    <div class="container px-4 mx-auto">
+        <div @class(['w-full', $getImageWidthClass()])>
+            {{$getImageMedia(['alt' => $imageTitle, 'class'=> 'w-full', 'loading' => 'lazy'])}}
+            @if($imageCopyright)
+                <small>&copy; {{$imageCopyright}}</small>
+            @endif
+        </div>
+    </div>
 </div>
--- resources/views/content-blocks/text-image.blade.php
+++ resources/views/content-blocks/text-image.blade.php
@@ -1,6 +1,5 @@
 @php
     $imagePositionClass = $getImageMedia && $imagePosition === 'right' ? 'md:flex-row-reverse' : '';
-    $imageSizes = '(max-width: 479px) 95vw, (min-width: 480px) and (max-width: 659px) 448px, (min-width: 660px) and (max-width: 819px) 334px, (min-width: 820px) and (max-width: 979px) 414px, (min-width: 980px) and (max-width: 1279px) 494px, (min-width: 1280px) 736px';
 @endphp
 
 <div class="py-20 section section--default">
@@ -8,7 +7,7 @@
         <div class="flex flex-wrap -mx-4 {{ $imagePositionClass }}">
             @if($getImageMedia)
                 <div class="w-full px-4 mb-4 md:w-1/2 md:mb-0">
-                    {{$getImageMedia(['alt' => $imageTitle, 'sizes' => $imageSizes])}}
+                    {{$getImageMedia(['class'=> 'w-full', 'loading' => 'lazy'])}}
 
                     @if($imageCopyright)
                         <small>&copy; {{$imageCopyright}}</small>
