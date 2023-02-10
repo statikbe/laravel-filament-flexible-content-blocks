c4b17af22f541e91bf8721c44e0d7a557d8e2fe7
sten <sten@users.noreply.github.com>
2023-02-10 22:02:52 +0000

    Fix styling

--- src/Models/Contracts/Linkable.php
+++ src/Models/Contracts/Linkable.php
@@ -2,12 +2,12 @@
 
 namespace Statikbe\FilamentFlexibleContentBlocks\Models\Contracts;
 
-    interface Linkable
-    {
-        /**
-         * Returns the URL of the view route of this model. This is used to create links from the model instance.
-         *
-         * @return string
-         */
-        public function getViewUrl(): string;
-    }
+interface Linkable
+{
+    /**
+     * Returns the URL of the view route of this model. This is used to create links from the model instance.
+     *
+     * @return string
+     */
+    public function getViewUrl(): string;
+}
