c6be492d58bdad12151389848f0202f935095864
sten <sten@users.noreply.github.com>
2023-02-13 09:54:35 +0000

    Fix styling

--- src/View/Components/OverviewCard.php
+++ src/View/Components/OverviewCard.php
@@ -1,21 +1,21 @@
 <?php
 
-    namespace Statikbe\FilamentFlexibleContentBlocks\View\Components;
+namespace Statikbe\FilamentFlexibleContentBlocks\View\Components;
 
     use Illuminate\View\Component;
-    use Statikbe\FilamentFlexibleContentBlocks\Models\Contracts\HasOverviewAttributes;
-    use Statikbe\FilamentFlexibleContentBlocks\Models\Contracts\Linkable;
 
-    class OverviewCard extends Component {
-
+    class OverviewCard extends Component
+    {
         public function __construct(
-            public ?string $title=null,
-            public ?string $description=null,
-            public ?string $image=null,
-            public ?string $url=null,
-        ){}
+            public ?string $title = null,
+            public ?string $description = null,
+            public ?string $image = null,
+            public ?string $url = null,
+        ) {
+        }
 
-        public function render() {
+        public function render()
+        {
             return view('filament-flexible-content-blocks::components.overview-card');
         }
     }
