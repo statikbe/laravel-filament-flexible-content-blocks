cf0b39f1d0fd6fefa68c3ee07aea9b841b03fe18
sten <sten@users.noreply.github.com>
2023-02-13 10:31:34 +0000

    Fix styling

--- src/View/Components/OverviewCard.php
+++ src/View/Components/OverviewCard.php
@@ -2,20 +2,20 @@
 
 namespace Statikbe\FilamentFlexibleContentBlocks\View\Components;
 
-    use Illuminate\View\Component;
+use Illuminate\View\Component;
 
-    class OverviewCard extends Component
-    {
-        public function __construct(
+class OverviewCard extends Component
+{
+    public function __construct(
             public ?string $title = null,
             public ?string $description = null,
             public ?string $image = null,
             public ?string $url = null,
         ) {
-        }
-
-        public function render()
-        {
-            return view('filament-flexible-content-blocks::components.overview-card');
-        }
     }
+
+    public function render()
+    {
+        return view('filament-flexible-content-blocks::components.overview-card');
+    }
+}
