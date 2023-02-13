3113fd9443e2a97a92ee86a026bf7bce75e3a078
Stijn Elskens <stijn@statik.be>
2023-02-13 13:23:34 +0100

    Update image components and use class blade directive

--- resources/views/content-blocks/image.blade.php
+++ resources/views/content-blocks/image.blade.php
@@ -1,19 +1,7 @@
-@php
-    $imagePositionClass = $imagePosition === 'right' ? 'md:flex-row-reverse' : '';
-    // if ($getImageWidthClass() === 'w-3/4') {
-    //     $imageWidthClass = 'md:w-3/4';
-    //  } else if ($getImageWidthClass() === 'w-1/2') {
-    //     $imageWidthClass = 'md:w-1/2';
-    //  } else if ($getImageWidthClass() === 'w-1/3') {
-    //     $imageWidthClass = 'md:w-1/3';
-    //  } else { // w-full
-    //     $imageWidthClass = '';
-    // }
-@endphp
-
 <div class="py-20 section section--default">
     <div class="container px-4 mx-auto">
-        <div @class(['w-full', $getImageWidthClass()])>
+        {{-- TODO: Make this cleaner for left, center, right? --}}
+        <div @class(['w-full', $getImageWidthClass(), '' => $imagePosition === 'left', 'mx-auto' => $imagePosition === 'center', 'ml-auto' => $imagePosition === 'right'])>
             {{$getImageMedia(['alt' => $imageTitle, 'class'=> 'w-full', 'loading' => 'lazy'])}}
             @if($imageCopyright)
                 <small>&copy; {{$imageCopyright}}</small>
