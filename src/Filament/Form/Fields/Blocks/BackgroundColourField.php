475c3956967d9e7620e6ce5fd23a507be8871240
sten <sten@users.noreply.github.com>
2023-02-15 22:34:52 +0000

    Fix styling

--- src/Filament/Form/Fields/Blocks/BackgroundColourField.php
+++ src/Filament/Form/Fields/Blocks/BackgroundColourField.php
@@ -2,24 +2,22 @@
 
 namespace Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Blocks;
 
-    use Filament\Forms\Components\Select;
-    use Statikbe\FilamentFlexibleContentBlocks\ContentBlocks\AbstractContentBlock;
-    use Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleBlocksConfig;
+use Filament\Forms\Components\Select;
+use Statikbe\FilamentFlexibleContentBlocks\ContentBlocks\AbstractContentBlock;
+use Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleBlocksConfig;
 
-    class BackgroundColourField extends Select
+class BackgroundColourField extends Select
+{
+    /**
+     * @param  class-string<AbstractContentBlock>  $blockClass
+     */
+    public static function create(string $blockClass, bool $required = true): static
     {
-        /**
-         * @param  class-string<AbstractContentBlock>  $blockClass
-         * @param  bool  $required
-         * @return static
-         */
-        public static function create(string $blockClass, bool $required = true): static
-        {
-            return static::make('background_colour')
-                ->label(trans('filament-flexible-content-blocks::filament-flexible-content-blocks.form_component.background_colour_lbl'))
-                ->options(FilamentFlexibleBlocksConfig::getBackgroundColoursSelectOptions($blockClass))
-                ->default(FilamentFlexibleBlocksConfig::getBackgroundColourDefault($blockClass))
-                ->disablePlaceholderSelection()
-                ->required($required);
-        }
+        return static::make('background_colour')
+            ->label(trans('filament-flexible-content-blocks::filament-flexible-content-blocks.form_component.background_colour_lbl'))
+            ->options(FilamentFlexibleBlocksConfig::getBackgroundColoursSelectOptions($blockClass))
+            ->default(FilamentFlexibleBlocksConfig::getBackgroundColourDefault($blockClass))
+            ->disablePlaceholderSelection()
+            ->required($required);
     }
+}
