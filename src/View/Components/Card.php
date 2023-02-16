57b7da0efd6f94a3d3e78eb52e6611606039e050
sten <sten@users.noreply.github.com>
2023-02-16 22:06:30 +0000

    Fix styling

--- src/View/Components/Card.php
+++ src/View/Components/Card.php
@@ -2,32 +2,29 @@
 
 namespace Statikbe\FilamentFlexibleContentBlocks\View\Components;
 
-    use Illuminate\View\Component;
-    use Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Blocks\Data\CallToActionData;
+use Illuminate\View\Component;
+use Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Blocks\Data\CallToActionData;
 
-    class Card extends Component
-    {
-        public ?string $titleUrl;
+class Card extends Component
+{
+    public ?string $titleUrl;
 
-        /**
-         * @param  string|null  $title
-         * @param  string|null  $description
-         * @param  string|null  $image
-         * @param  CallToActionData[]|null  $callToActions
-         */
-        public function __construct(
+    /**
+     * @param  CallToActionData[]|null  $callToActions
+     */
+    public function __construct(
             public ?string $title = null,
             public ?string $description = null,
             public ?string $image = null,
             public ?array $callToActions = [],
         ) {
-            if ($this->callToActions && ! empty($this->callToActions)) {
-                $this->titleUrl = $this->callToActions[0]->url;
-            }
-        }
-
-        public function render()
-        {
-            return view('filament-flexible-content-blocks::components.card');
+        if ($this->callToActions && ! empty($this->callToActions)) {
+            $this->titleUrl = $this->callToActions[0]->url;
         }
     }
+
+    public function render()
+    {
+        return view('filament-flexible-content-blocks::components.card');
+    }
+}
