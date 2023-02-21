9a483aaef3692216f8e6a3789804e580e2e76371
sten <sten@users.noreply.github.com>
2023-02-21 14:06:11 +0000

    Fix styling

--- src/Models/Contracts/HasCode.php
+++ src/Models/Contracts/HasCode.php
@@ -2,14 +2,14 @@
 
 namespace Statikbe\FilamentFlexibleContentBlocks\Models\Contracts;
 
-    use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Builder;
 
-    /**
-     * A code is a unique field to identify a page
-     *
-     * @property string $code
-     */
-    interface HasCode
-    {
-        public function scopeCode(Builder $query, string $code): Builder;
-    }
+/**
+ * A code is a unique field to identify a page
+ *
+ * @property string $code
+ */
+interface HasCode
+{
+    public function scopeCode(Builder $query, string $code): Builder;
+}
