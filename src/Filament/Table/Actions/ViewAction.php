7c8f7084b6c2945412bf86242510c854a8b7c5f4
sten <sten@users.noreply.github.com>
2023-02-21 22:33:07 +0000

    Fix styling

--- src/Filament/Table/Actions/ViewAction.php
+++ src/Filament/Table/Actions/ViewAction.php
@@ -2,29 +2,29 @@
 
 namespace Statikbe\FilamentFlexibleContentBlocks\Filament\Table\Actions;
 
-    use Filament\Tables\Actions\Action;
-    use Statikbe\FilamentFlexibleContentBlocks\Models\Contracts\Linkable;
+use Filament\Tables\Actions\Action;
+use Statikbe\FilamentFlexibleContentBlocks\Models\Contracts\Linkable;
 
-    class ViewAction extends Action
+class ViewAction extends Action
+{
+    public static function getDefaultName(): ?string
     {
-        public static function getDefaultName(): ?string
-        {
-            return 'view';
-        }
-
-        protected function setUp(): void
-        {
-            parent::setUp();
-
-            $this->label(trans('filament-flexible-content-blocks::filament-flexible-content-blocks.table_action.view_page_lbl'));
-
-            $this->color('secondary');
-
-            $this->icon('heroicon-s-eye');
-
-            $this->disableForm();
-
-            $this->url(fn (Linkable $record) => $record->getPreviewUrl())
-                ->openUrlInNewTab();
-        }
+        return 'view';
     }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->label(trans('filament-flexible-content-blocks::filament-flexible-content-blocks.table_action.view_page_lbl'));
+
+        $this->color('secondary');
+
+        $this->icon('heroicon-s-eye');
+
+        $this->disableForm();
+
+        $this->url(fn (Linkable $record) => $record->getPreviewUrl())
+            ->openUrlInNewTab();
+    }
+}
