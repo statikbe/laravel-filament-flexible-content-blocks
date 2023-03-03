4815f74ce6d51aec2bc2464ea819bd20202d482f
sten <sten@users.noreply.github.com>
2023-03-03 12:01:11 +0000

    Fix styling

--- src/ContentBlocks/Concerns/HasImageConversionType.php
+++ src/ContentBlocks/Concerns/HasImageConversionType.php
@@ -2,47 +2,47 @@
 
 namespace Statikbe\FilamentFlexibleContentBlocks\ContentBlocks\Concerns;
 
-    use Illuminate\Support\Str;
-    use Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Blocks\ImageConversionTypeField;
+use Illuminate\Support\Str;
+use Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Blocks\ImageConversionTypeField;
 
-    trait HasImageConversionType
+trait HasImageConversionType
+{
+    public ?string $imageConversion;
+
+    protected function setImageConversionType(array $blockData): void
     {
-        public ?string $imageConversion;
+        $this->imageConversion = $blockData[ImageConversionTypeField::FIELD] ?? static::getImageConversionTypeDefault();
+    }
 
-        protected function setImageConversionType(array $blockData): void
-        {
-            $this->imageConversion = $blockData[ImageConversionTypeField::FIELD] ?? static::getImageConversionTypeDefault();
-        }
+    /**
+     * @return array<string, string>
+     */
+    public static function getImageConversionTypeOptions(): array
+    {
+        return [
+            static::CONVERSION_CROP => trans('filament-flexible-content-blocks::filament-flexible-content-blocks.form_component.image_conversion_type_'.Str::lower(static::CONVERSION_CROP)),
+            static::CONVERSION_CONTAIN => trans('filament-flexible-content-blocks::filament-flexible-content-blocks.form_component.image_conversion_type_'.Str::lower(static::CONVERSION_CONTAIN)),
+        ];
+    }
 
-        /**
-         * @return array<string, string>
-         */
-        public static function getImageConversionTypeOptions(): array
-        {
-            return [
-                static::CONVERSION_CROP => trans('filament-flexible-content-blocks::filament-flexible-content-blocks.form_component.image_conversion_type_'.Str::lower(static::CONVERSION_CROP)),
-                static::CONVERSION_CONTAIN => trans('filament-flexible-content-blocks::filament-flexible-content-blocks.form_component.image_conversion_type_'.Str::lower(static::CONVERSION_CONTAIN)),
-            ];
-        }
+    /**
+     * @return string[]
+     */
+    public static function getImageConversionTypeDefault(): array
+    {
+        return [
+            static::CONVERSION_CROP,
+        ];
+    }
 
-        /**
-         * @return string[]
-         */
-        public static function getImageConversionTypeDefault(): array
-        {
-            return [
-                static::CONVERSION_CROP,
-            ];
-        }
-
-        public function getImageConversionType(string $conversion = null): ?string
-        {
-            if ($conversion) {
-                return $conversion;
-            } elseif ($this->imageConversion) {
-                return $this->imageConversion;
-            } else {
-                return collect(static::getImageConversionTypeDefault())->first();
-            }
+    public function getImageConversionType(string $conversion = null): ?string
+    {
+        if ($conversion) {
+            return $conversion;
+        } elseif ($this->imageConversion) {
+            return $this->imageConversion;
+        } else {
+            return collect(static::getImageConversionTypeDefault())->first();
         }
     }
+}
