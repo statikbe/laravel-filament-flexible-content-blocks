88d97590fb48d35dc01f52ddc1b4b7fe1b91a179
Stijn Elskens <stijn@statik.be>
2023-03-16 10:55:57 +0100

    add extra classes in templates

--- resources/views/content-blocks/quote.blade.php
+++ resources/views/content-blocks/quote.blade.php
@@ -1,12 +1,12 @@
 <div class="py-12 section section--default">
     <div class="container px-4 mx-auto">
-        <blockquote class="pl-6 border-l-2 border-gray-200">
+        <blockquote class="pl-6 border-l-2 border-gray-200 quote">
             <div class="text-2xl">
                 {!! $quote !!}
             </div>
             
             @if($author)
-                <footer><small>{{$author}}</small></footer>
+                <footer class="quote__footer"><small>{{$author}}</small></footer>
             @endif
         </blockquote>
     </div>
