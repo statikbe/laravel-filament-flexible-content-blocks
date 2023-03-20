dc009dfe8f0b20b2cdca2a6759b57a6b001d3f65
sten <sten@users.noreply.github.com>
2023-03-20 22:13:49 +0000

    Fix styling

--- src/Filament/Form/Fields/Concerns/HasTranslatableHint.php
+++ src/Filament/Form/Fields/Concerns/HasTranslatableHint.php
@@ -1,6 +1,6 @@
 <?php
 
-    namespace Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Concerns;
+namespace Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Concerns;
 
     use Filament\Forms\Components\Field;
     use Illuminate\Database\Eloquent\Model;
@@ -8,20 +8,23 @@
     /**
      * Checks if the field is translatable and adds a default translatable hint, if no other hint is provided.
      */
-    trait HasTranslatableHint {
+    trait HasTranslatableHint
+    {
         public function addsTranslatableHint(): static
         {
-            $this->hint = function(?Model $record, Field $component){
-                if($record && isset($record->translatable) && in_array($component->getName(), $record->translatable)){
+            $this->hint = function (?Model $record, Field $component) {
+                if ($record && isset($record->translatable) && in_array($component->getName(), $record->translatable)) {
                     return trans('filament-flexible-content-blocks::filament-flexible-content-blocks.form_component.translatable_hint');
                 }
+
                 return null;
             };
 
-            $this->hintIcon = function(?Model $record, Field $component){
-                if($record && isset($record->translatable) && in_array($component->getName(), $record->translatable)){
+            $this->hintIcon = function (?Model $record, Field $component) {
+                if ($record && isset($record->translatable) && in_array($component->getName(), $record->translatable)) {
                     return 'heroicon-s-translate';
                 }
+
                 return null;
             };
 
