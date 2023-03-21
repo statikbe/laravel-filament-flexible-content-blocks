a80ca0571e242dbbdaff80caef57f17cc654ad76
Kristof <kristof@statik.be>
2023-03-21 09:09:29 +0100

    Removed obsolete add conversion function in hasImage

--- src/ContentBlocks/Concerns/HasImage.php
+++ src/ContentBlocks/Concerns/HasImage.php
@@ -79,7 +79,6 @@ trait HasImage
             ->fit($fitType, $width, $height)
             ->format(Manipulations::FORMAT_WEBP);
         FilamentFlexibleBlocksConfig::mergeConfiguredFlexibleBlockImageConversion(static::class, static::getName(), $conversionName, $conversion);
-        FilamentFlexibleBlocksConfig::addExtraFlexibleBlockImageConversions(static::class, static::getName(), $record);
 
         return $conversion;
     }
