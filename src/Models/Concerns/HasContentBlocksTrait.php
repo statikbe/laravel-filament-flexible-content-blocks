62cf7eb1517014178c4af7c23c3b6510116f2d9b
s <>
2023-05-04 12:05:15 +0200

    remove html special chars from searchable block content if html needs to be stripped

--- src/Models/Concerns/HasContentBlocksTrait.php
+++ src/Models/Concerns/HasContentBlocksTrait.php
@@ -46,7 +46,7 @@ trait HasContentBlocksTrait
 
         if ($stripHtml) {
             $searchableContent = $searchableContent->map(function ($item) {
-                return htmlspecialchars_decode(strip_tags($item));
+                return html_entity_decode(htmlspecialchars_decode(strip_tags($item)));
             });
 
             return $searchableContent->implode(" \n ");
