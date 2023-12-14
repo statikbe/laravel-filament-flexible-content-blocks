2166de42cb265292a549f582401b8a645e24e243
Sten Govaerts <sten.govaerts@gmail.com>
2023-12-14 17:45:03 +0100

    fix bug in saving of translatable media field

--- src/Filament/Pages/CreateRecord/Concerns/TranslatableWithMedia.php
+++ src/Filament/Pages/CreateRecord/Concerns/TranslatableWithMedia.php
@@ -3,11 +3,84 @@
 namespace Statikbe\FilamentFlexibleContentBlocks\Filament\Pages\CreateRecord\Concerns;
 
 use Filament\Resources\Pages\CreateRecord\Concerns\Translatable;
+use Filament\Support\Exceptions\Halt;
 use Illuminate\Support\Arr;
+use Illuminate\Validation\ValidationException;
 
 trait TranslatableWithMedia
 {
     use Translatable;
+
+    public function create(bool $another = false): void
+    {
+        $this->authorizeAccess();
+
+        try {
+            $this->callHook('beforeValidate');
+
+            $data = [
+                $this->activeLocale => $this->form->getState(),
+            ];
+
+            $this->callHook('afterValidate');
+
+            $originalActiveLocale = $this->activeLocale;
+
+            $translatableAttributes = $this->getTranslatedAttributesWithTranslatableMedia();
+
+            $nonTranslatableData = Arr::except(
+                $this->data[$originalActiveLocale] ?? [],
+                $translatableAttributes,
+            );
+
+            foreach ($this->getTranslatableLocales() as $locale) {
+                if ($locale === $originalActiveLocale) {
+                    continue;
+                }
+
+                try {
+                    $this->setActiveLocale($locale);
+
+                    $this->data[$locale] = array_merge(
+                        $this->data[$locale] ?? [],
+                        $nonTranslatableData,
+                    );
+
+                    $this->callHook('beforeValidate');
+
+                    $data[$locale] = $this->form->getState();
+
+                    $this->callHook('afterValidate');
+                } catch (ValidationException $exception) {
+                    // If the validation fails for a non-active locale,
+                    // we'll just ignore it and continue, since it's
+                    // likely that the user hasn't filled out the
+                    // required fields for that locale.
+                }
+            }
+
+            $this->setActiveLocale($originalActiveLocale);
+
+            foreach ($data as $locale => $localeData) {
+                if ($locale !== $this->activeLocale) {
+                    $localeData = Arr::only(
+                        $localeData,
+                        $translatableAttributes,
+                    );
+                }
+
+                $data[$locale] = $this->mutateFormDataBeforeCreate($localeData);
+            }
+
+            /** @internal Read the DocBlock above the following method. */
+            $this->createRecordAndCallHooks($data);
+        } catch (Halt $exception) {
+            return;
+        }
+
+        /** @internal Read the DocBlock above the following method. */
+        $this->sendCreatedNotificationAndRedirect(shouldCreateAnotherInsteadOfRedirecting: $another);
+    }
 
     /**
      * This function is overridden to be able to update the state of translatable media when the form locale switches.
@@ -24,13 +97,7 @@ trait TranslatableWithMedia
             return;
         }
 
-        $record = app(static::getModel());
-        $translatableAttributes = $record->getTranslatableAttributes();
-        if (method_exists($record, 'getTranslatableMediaCollections')) {
-            $translatableAttributes = array_merge($translatableAttributes, $record->getTranslatableMediaCollections());
-        } else {
-            throw new \BadMethodCallException('The model does not implement the HasTranslatableMedia interface. If this is not implemented translated images will not change when the language switch changes.');
-        }
+        $translatableAttributes = $this->getTranslatedAttributesWithTranslatableMedia();
 
         $this->data[$newActiveLocale] = array_merge(
             $this->data[$newActiveLocale] ?? [],
@@ -44,5 +111,17 @@ trait TranslatableWithMedia
             $this->data[$this->oldActiveLocale] ?? [],
             $translatableAttributes,
         );
+    }
+
+    private function getTranslatedAttributesWithTranslatableMedia(): array {
+        $record = app(static::getModel());
+        $translatableAttributes = $record->getTranslatableAttributes();
+        if (method_exists($record, 'getTranslatableMediaCollections')) {
+            $translatableAttributes = array_merge($translatableAttributes, $record->getTranslatableMediaCollections());
+        } else {
+            throw new \BadMethodCallException('The model does not implement the HasTranslatableMedia interface. If this is not implemented translated images will not change when the language switch changes.');
+        }
+
+        return $translatableAttributes;
     }
 }
--- src/Filament/Pages/EditRecord/Concerns/TranslatableWithMedia.php
+++ src/Filament/Pages/EditRecord/Concerns/TranslatableWithMedia.php
@@ -3,6 +3,7 @@
 namespace Statikbe\FilamentFlexibleContentBlocks\Filament\Pages\EditRecord\Concerns;
 
 use Filament\Resources\Pages\EditRecord\Concerns\Translatable;
+use Filament\Support\Exceptions\Halt;
 use Illuminate\Support\Arr;
 use Illuminate\Validation\ValidationException;
 use Statikbe\FilamentFlexibleContentBlocks\Models\Contracts\HasTranslatableMedia;
@@ -10,6 +11,44 @@ use Statikbe\FilamentFlexibleContentBlocks\Models\Contracts\HasTranslatableMedia
 trait TranslatableWithMedia
 {
     use Translatable;
+
+    public function save(bool $shouldRedirect = true): void
+    {
+        $this->authorizeAccess();
+
+        $originalActiveLocale = $this->activeLocale;
+
+        try {
+            /** @internal Read the DocBlock above the following method. */
+            $this->validateFormAndUpdateRecordAndCallHooks();
+
+            $nonTranslatableData = Arr::except(
+                $this->data[$originalActiveLocale] ?? [],
+                $this->getTranslatedAttributesWithTranslatableMedia()
+            );
+
+            $otherTranslatableLocales = Arr::except($this->getTranslatableLocales(), $originalActiveLocale);
+
+            foreach ($otherTranslatableLocales as $locale) {
+                $this->setActiveLocale($locale);
+
+                $this->data[$locale] = array_merge(
+                    $this->data[$locale] ?? [],
+                    $nonTranslatableData,
+                );
+
+                /** @internal Read the DocBlock above the following method. */
+                $this->validateFormAndUpdateRecordAndCallHooks();
+            }
+        } catch (Halt $exception) {
+            return;
+        }
+
+        $this->setActiveLocale($originalActiveLocale);
+
+        /** @internal Read the DocBlock above the following method. */
+        $this->sendSavedNotificationAndRedirect(shouldRedirect: $shouldRedirect);
+    }
 
     /**
      * This function is overridden to be able to update the state of translatable media when the form locale switches.
@@ -40,12 +79,7 @@ trait TranslatableWithMedia
             return;
         }
 
-        $translatableAttributes = app(static::getModel())->getTranslatableAttributes();
-        if (method_exists($this->getRecord(), 'getTranslatableMediaCollections')) {
-            $translatableAttributes = array_merge($translatableAttributes, $this->getRecord()->getTranslatableMediaCollections());
-        } else {
-            throw new \BadMethodCallException('The model does not implement the HasTranslatableMedia interface. If this is not implemented translated images will not change when the language switch changes.');
-        }
+        $translatableAttributes = $this->getTranslatedAttributesWithTranslatableMedia();
 
         $this->data[$newActiveLocale] = array_merge(
             $this->data[$newActiveLocale] ?? [],
@@ -59,5 +93,16 @@ trait TranslatableWithMedia
             $this->data[$this->oldActiveLocale] ?? [],
             $translatableAttributes,
         );
+    }
+
+    private function getTranslatedAttributesWithTranslatableMedia(): array {
+        $translatableAttributes = app(static::getModel())->getTranslatableAttributes();
+        if (method_exists($this->getRecord(), 'getTranslatableMediaCollections')) {
+            $translatableAttributes = array_merge($translatableAttributes, $this->getRecord()->getTranslatableMediaCollections());
+        } else {
+            throw new \BadMethodCallException('The model does not implement the HasTranslatableMedia interface. If this is not implemented translated images will not change when the language switch changes.');
+        }
+
+        return $translatableAttributes;
     }
 }
