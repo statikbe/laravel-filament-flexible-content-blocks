3fdaf533fe90971773b03774299bb07ddad614e3
Sten Govaerts <sten.govaerts@gmail.com>
2024-01-24 16:06:39 +0100

    fix bug in block image field

--- src/Filament/Form/Fields/Blocks/BlockSpatieMediaLibraryFileUpload.php
+++ src/Filament/Form/Fields/Blocks/BlockSpatieMediaLibraryFileUpload.php
@@ -5,11 +5,14 @@ namespace Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Blocks;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Concerns\HasImageEditor;
 use Statikbe\FilamentFlexibleContentBlocks\Models\Contracts\HasContentBlocks;
+use Closure;
 
 /**
  * An extension to the spatie media-library field of Filament to also allow to save the UUID to a block.
@@ -28,31 +31,96 @@ class BlockSpatieMediaLibraryFileUpload extends SpatieMediaLibraryFileUpload
 
         //Extend the media library load logic to also look at the given state (in our case the uuids of the images)
         $this->loadStateFromRelationshipsUsing(static function (SpatieMediaLibraryFileUpload $component, HasMedia $record, $state): void {
-            if (is_string($state)) {
-                $state = [$state];
+            $stateUuids = [];
+            if(is_string($state)){
+                $stateUuids = [$state];
+            }
+            else if(is_array($state) && !empty($state)) {
+                if(Str::isUuid(array_keys($state)[0])) {
+                    $stateUuids = array_keys($state);
+                }
+                else {
+                    $stateUuids = array_values($state);
+                }
             }
 
-            $files = [];
-            if (is_array($state)) {
-                $files = $record->load('media')->getMedia($component->getCollection())
-                    ->whereIn('uuid', $state)
-                    ->when(
-                        ! $component->isMultiple(),
-                        fn (Collection $files): Collection => $files->take(1),
-                    )
-                    ->mapWithKeys(function (Media $file): array {
-                        $uuid = $file->getAttributeValue('uuid');
+            /** @var Model&HasMedia $record */
+            $media = $record->load('media')->getMedia($component->getCollection())
+                ->whereIn('uuid', $stateUuids)
+                ->when(
+                    ! $component->isMultiple(),
+                    fn (Collection $media): Collection => $media->take(1),
+                )
+                ->when(
+                    $component->hasMediaFilter(),
+                    fn (Collection $media) => $component->filterMedia($media)
+                )
+                ->mapWithKeys(function (Media $media): array {
+                    $uuid = $media->getAttributeValue('uuid');
 
-                        return [$uuid => $uuid];
-                    })
-                    ->toArray();
-            }
+                    return [$uuid => $uuid];
+                })
+                ->toArray();
 
-            $component->state($files);
+            $component->state($media);
         });
 
         //Make sure the uuid of the image is added to the form data
         $this->dehydrated(true);
+    }
+
+    public function getState(): mixed
+    {
+        $state = parent::getState();
+        //transform strings to arrays since the upload field expects an array of UUIDs:
+        if($state && !is_array($state)){
+            $state = array_filter([$state]);
+        }
+        return $state;
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function getValidationRules(): array
+    {
+        $rules = [
+            $this->getRequiredValidationRule(),
+            'array',
+        ];
+
+        if (filled($count = $this->getMaxFiles())) {
+            $rules[] = "max:{$count}";
+        }
+
+        if (filled($count = $this->getMinFiles())) {
+            $rules[] = "min:{$count}";
+        }
+
+        $rules[] = function (string $attribute, array|string $value, Closure $fail): void {
+            if(is_string($value)){
+                $value = [$value];
+            }
+
+            $files = array_filter($value, fn (TemporaryUploadedFile | string $file): bool => $file instanceof TemporaryUploadedFile);
+
+            $name = $this->getName();
+
+            $validator = Validator::make(
+                [$name => $files],
+                ["{$name}.*" => ['file', ...parent::getValidationRules()]],
+                [],
+                ["{$name}.*" => $this->getValidationAttribute()],
+            );
+
+            if (! $validator->fails()) {
+                return;
+            }
+
+            $fail($validator->errors()->first());
+        };
+
+        return $rules;
     }
 
     public function deleteAbandonedFiles(): void
