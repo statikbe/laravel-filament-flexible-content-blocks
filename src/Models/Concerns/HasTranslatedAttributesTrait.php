a1d1f73f0cd2aa64ab7660b3888adeb5690d3934
Sten Govaerts <sten.govaerts@gmail.com>
2024-03-21 12:35:04 +0100

    add translated media trait

--- src/Models/Concerns/HasTranslatedAttributesTrait.php
+++ src/Models/Concerns/HasTranslatedAttributesTrait.php
@@ -11,6 +11,7 @@ use Spatie\Translatable\HasTranslations;
 trait HasTranslatedAttributesTrait
 {
     use HasTranslations;
+    use HasTranslatedMediaTrait;
 
     /**
      * @var string[]
@@ -30,46 +31,5 @@ trait HasTranslatedAttributesTrait
         );
     }
 
-    public function mergeTranslatableMediaCollection(array $translatableMediaCollections): void
-    {
-        $this->translatableMediaCollections = array_merge(
-            $this->getTranslatableMediaCollections(),
-            $translatableMediaCollections
-        );
-    }
 
-    /**
-     * @return string[]
-     */
-    public function getTranslatableMediaCollections(): array
-    {
-        return $this->translatableMediaCollections;
-    }
-
-    /**
-     * Retrieves the media UUIDs per media collection. Returns an array with the media collection as key and a list of
-     * media UUIDs as values.
-     *
-     * @return array<string, array<string>>
-     */
-    public function getTranslatableMediaUuidsPerMediaCollection(?string $locale = null): array
-    {
-        if (empty($this->getTranslatableMediaCollections())) {
-            return [];
-        }
-
-        if (! $locale) {
-            $locale = app()->getLocale();
-        }
-
-        $translatableMedia = $this->media()
-            ->whereIn('collection_name', $this->getTranslatableMediaCollections())
-            ->where('custom_properties->locale', $locale)
-            ->select('collection_name', 'uuid')
-            ->get();
-
-        return $translatableMedia->mapToGroups(function ($item, int $key) {
-            return [$item->collection_name => $item->uuid];
-        })->toArray();
-    }
 }
---
+++ src/Models/Concerns/HasTranslatedMediaTrait.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Statikbe\FilamentFlexibleContentBlocks\Models\Concerns;
+
+/**
+ * @property array $translatableMediaCollections
+ */
+trait HasTranslatedMediaTrait
+{
+    /**
+     * @var string[]
+     */
+    public $translatableMediaCollections = [];
+
+    public function mergeTranslatableMediaCollection(array $translatableMediaCollections): void
+    {
+        $this->translatableMediaCollections = array_merge(
+            $this->getTranslatableMediaCollections(),
+            $translatableMediaCollections
+        );
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getTranslatableMediaCollections(): array
+    {
+        return $this->translatableMediaCollections;
+    }
+
+    /**
+     * Retrieves the media UUIDs per media collection. Returns an array with the media collection as key and a list of
+     * media UUIDs as values.
+     *
+     * @return array<string, array<string>>
+     */
+    public function getTranslatableMediaUuidsPerMediaCollection(?string $locale = null): array
+    {
+        if (empty($this->getTranslatableMediaCollections())) {
+            return [];
+        }
+
+        if (! $locale) {
+            $locale = app()->getLocale();
+        }
+
+        $translatableMedia = $this->media()
+            ->whereIn('collection_name', $this->getTranslatableMediaCollections())
+            ->where('custom_properties->locale', $locale)
+            ->select('collection_name', 'uuid')
+            ->get();
+
+        return $translatableMedia->mapToGroups(function ($item, int $key) {
+            return [$item->collection_name => $item->uuid];
+        })->toArray();
+    }
+}
