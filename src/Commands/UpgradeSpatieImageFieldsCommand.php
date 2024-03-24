07ce35757e6e48541dd9e11560b866a9dc495c43
Sten Govaerts <sten.govaerts@gmail.com>
2024-03-24 22:43:25 +0100

    fix bug in upgrade command to copy media that already has a block

--- src/Commands/UpgradeSpatieImageFieldsCommand.php
+++ src/Commands/UpgradeSpatieImageFieldsCommand.php
@@ -6,11 +6,26 @@ use Illuminate\Console\Command;
 use Illuminate\Console\Concerns\PromptsForMissingInput as PromptsForMissingInputTrait;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\BlockIdField;
 use Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks;
 use Statikbe\FilamentFlexibleContentBlocks\Models\Contracts\HasContentBlocks;
 
+/**
+ * This command upgrades the content blocks data model to support the v1.0 features of this package.
+ * Hence, this command should only be executed if you are upgrading from a version of this package < 1.0.
+ *
+ * The command adds `block_id` variables to each block with a unique UUID that is used to link image fields to a block.
+ * Spatie Medialibrary uses a polymorph relationship with the model that has the content blocks. But this makes it
+ * difficult to link n images to different blocks of 1 model. The command adds the block id to the custom properties of
+ * the media record. This data is then used to link a media record to a specific block.
+ *
+ * The versions < 1.0 also had issues with copying the content blocks to different locales via the action. Namely, the
+ * the whole content blocks data was just copied to the different locales. This resulted in lost media for all locales,
+ * if the media was removed in one of the locales. The command also corrects this and makes a copy of the image in blocks,
+ * to make sure each image is uniaue per block per locale.
+ */
 class UpgradeSpatieImageFieldsCommand extends Command implements PromptsForMissingInput
 {
     use PromptsForMissingInputTrait;
@@ -39,13 +54,13 @@ class UpgradeSpatieImageFieldsCommand extends Command implements PromptsForMissi
                 if (isset($model->translatable) && in_array('content_blocks', $model->translatable)) {
                     foreach (FilamentFlexibleContentBlocks::getLocales() as $locale) {
                         $contentBlocks = $model->getTranslation('content_blocks', $locale);
-                        $upgradedContentBlocks = $this->upgradeContentBlocks($contentBlocks);
+                        $upgradedContentBlocks = $this->upgradeContentBlocks($contentBlocks, $model);
 
                         $model->setTranslation('content_blocks', $locale, $upgradedContentBlocks);
                         $model->save();
                     }
                 } else {
-                    $model->content_blocks = $this->upgradeContentBlocks($model->content_blocks);
+                    $model->content_blocks = $this->upgradeContentBlocks($model->content_blocks, $model);
 
                     //save upgrade
                     $model->save();
@@ -61,7 +76,7 @@ class UpgradeSpatieImageFieldsCommand extends Command implements PromptsForMissi
     }
 
     // Generate function
-    public function upgradeContentBlocks(array $contentBlocks): array
+    public function upgradeContentBlocks(array $contentBlocks, Model $model): array
     {
         foreach ($contentBlocks as &$block) {
             //add block id to each block:
@@ -71,7 +86,7 @@ class UpgradeSpatieImageFieldsCommand extends Command implements PromptsForMissi
 
             foreach ($this->imageFields as $imageField) {
                 if (isset($block['data'][$imageField]) && $block['data'][$imageField]) {
-                    $this->updateMedia($block['data'][$imageField], $block['data'][BlockIdField::FIELD]);
+                    $block['data'][$imageField] = $this->updateMedia($block['data'][$imageField], $block['data'][BlockIdField::FIELD], $model);
                 }
             }
 
@@ -83,7 +98,7 @@ class UpgradeSpatieImageFieldsCommand extends Command implements PromptsForMissi
                             $card[BlockIdField::FIELD] = BlockIdField::generateBlockId();
                             foreach ($this->imageFields as $imageField) {
                                 if (isset($card[$imageField]) && $card[$imageField]) {
-                                    $this->updateMedia($card[$imageField], $card[BlockIdField::FIELD]);
+                                    $card[$imageField] = $this->updateMedia($card[$imageField], $card[BlockIdField::FIELD], $model);
                                 }
                             }
                         }
@@ -95,16 +110,25 @@ class UpgradeSpatieImageFieldsCommand extends Command implements PromptsForMissi
         return $contentBlocks;
     }
 
-    private function updateMedia(?string $mediaUuid, string $blockId)
+    private function updateMedia(?string $mediaUuid, string $blockId, Model $model): string
     {
         if ($mediaUuid) {
             /* @var Media $media */
             $media = Media::findByUuid($mediaUuid);
             if ($media) {
+                //Check if there is already a block assigned. If so, this means it is already used by another block.
+                //This is possible because of the old copy content blocks button to other locales, that did not copy media for each locale.
+                if($media->getCustomProperty('block')){
+                    //make a copy of the media:
+                    $media = $media->copy($model, $media->collection_name, $media->disk);
+                }
                 //add block id as custom property to media:
                 $media->setCustomProperty('block', $blockId);
                 $media->save();
+
+                return $media->uuid;
             }
         }
+        return $mediaUuid;
     }
 }
