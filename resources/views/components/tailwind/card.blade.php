5132d9a45f228e704e07e90f397691515c21752a
Caroline Boeykens <caroline@statik.be>
2024-05-23 16:13:01 +0200

    cards styling

--- resources/views/components/tailwind/card.blade.php
+++ resources/views/components/tailwind/card.blade.php
@@ -2,34 +2,34 @@
     /* @var \Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Blocks\Data\CardData $card */
 @endphp
 
-<div class="relative transition-all duration-300 ease-out bg-white group card @if($isFullyClickable()) hover:shadow-md @endif">
+<div class="relative transition duration-300 ease-out bg-white group @if($isFullyClickable()) hover:shadow-md @endif">
     @if(!$slot->isEmpty())
         {{-- Image slot --}}
-        {{$slot}}
+        {{ $slot }}
     @elseif($card->hasImage())
         @if($card->imageHtml)
             {!! $card->imageHtml !!}
         @elseif($card->imageUrl)
-            <img src="{{$card->imageUrl}}"
-                 @if($card->title) alt="{{Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks::replaceParameters($card->title)}}" @endif class="card__image" />
+            <img src="{{ $card->imageUrl }}" class="w-full"
+                 @if($card->title)alt="{{Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks::replaceParameters($card->title)}}"@endif/>
         @endif
     @endif
 
-    <div class="p-4 prose sm:p-6 max-w-none">
+    <div class="p-4 sm:p-6">
         @if($card->title)
-            <h3 class="card__title">
+            <h3>
                 @if($getTitleUrl())<a href="{{$getTitleUrl()}}">@endif
-                    {{$card->title}}
+                    {{ $card->title }}
                 @if($getTitleUrl())</a>@endif
             </h3>
         @endif
 
         @if($card->text)
-            <div class="card__description">{!! Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks::replaceParameters($card->text) !!}</div>
+            <div>{!! Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks::replaceParameters($card->text) !!}</div>
         @endif
 
         @if($card->callToActions)
-            <div class="flex flex-wrap gap-4">
+            <div class="flex flex-wrap gap-4 mt-4">
                 @foreach($card->callToActions as $callToAction)
                     <x-flexible-call-to-action :data="$callToAction" :isFullyClickable="$isFullyClickable()"></x-flexible-call-to-action>
                 @endforeach
--- resources/views/content-blocks/tailwind/cards.blade.php
+++ resources/views/content-blocks/tailwind/cards.blade.php
@@ -1,18 +1,16 @@
-<div class="relative py-12 {{ $getBackgroundColourClass() }}">
-    <div class="container px-4 mx-auto">
+<div class="section {{ $getBackgroundColourClass() }}">
+    <div class="container">
         @if($title)
-            <div class="mb-6 prose max-w-none">
-                <h2>{{$replaceParameters($title)}}</h2>
-            </div>
+            <h2>{{$replaceParameters($title)}}</h2>
         @endif
-        <div class="grid sm:grid-cols-2 md:grid-cols-{{$gridColumns ?? 3}} gap-4">
+        <div @class(['grid gap-4 ', 'sm:grid-cols-2 md:grid-cols-' . $gridColumns => $gridColumns > 1])>
             @foreach($cards as $card)
                 @php
                      /* @var \Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Blocks\Data\CardData $card */
                 @endphp
 
                 <x-flexible-card :data="$card">
-                    {!! $getCardImageMedia($card->imageId, $card->title) !!}
+                    {!! $getCardImageMedia($card->imageId, $card->title, false, ['class' => 'w-full']) !!}
                 </x-flexible-card>
             @endforeach
         </div>
