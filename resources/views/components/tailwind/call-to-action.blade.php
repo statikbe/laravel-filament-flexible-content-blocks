522a3eb57d13693412252041940f404af61c430a
Sten Govaerts <sten.govaerts@gmail.com>
2024-12-02 15:19:23 +0100

    Fix 404 on missing model in CTA.

--- resources/views/components/tailwind/call-to-action.blade.php
+++ resources/views/components/tailwind/call-to-action.blade.php
@@ -2,15 +2,17 @@
     /* @var \Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Blocks\Data\CallToActionData $callToAction */
 @endphp
 
-<div {{$attributes}}>
-    <a href="{{$callToAction->url}}"
-    @if($callToAction->label) title="{{Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks::replaceParameters($callToAction->label)}}" @endif
-    class="@if($callToAction->buttonStyle) {{$callToAction->buttonStyle}} @endif  @if($isFullyClickable) before:absolute before:inset-0 @endif"
-    @if($callToAction->openNewWindow) target="_blank" rel="noopener noreferrer" @endif>
-        @if($callToAction->label)
-            {{Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks::replaceParameters($callToAction->label)}}
-        @else
-            &xrarr;
-        @endif
-    </a>
-</div>
+@if($callToAction->url)
+    <div {{$attributes}}>
+        <a href="{{$callToAction->url}}"
+        @if($callToAction->label) title="{{Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks::replaceParameters($callToAction->label)}}" @endif
+        class="@if($callToAction->buttonStyle) {{$callToAction->buttonStyle}} @endif  @if($isFullyClickable) before:absolute before:inset-0 @endif"
+        @if($callToAction->openNewWindow) target="_blank" rel="noopener noreferrer" @endif>
+            @if($callToAction->label)
+                {{Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks::replaceParameters($callToAction->label)}}
+            @else
+                &xrarr;
+            @endif
+        </a>
+    </div>
+@endif
--- src/Filament/Form/Fields/Blocks/Data/CallToActionData.php
+++ src/Filament/Form/Fields/Blocks/Data/CallToActionData.php
@@ -3,7 +3,9 @@
 namespace Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Blocks\Data;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Log;
 use Statikbe\FilamentFlexibleContentBlocks\Exceptions\CallToActionNotDefinedException;
 use Statikbe\FilamentFlexibleContentBlocks\Exceptions\LinkableModelNotFoundException;
 use Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\Blocks\CallToActionField;
@@ -13,7 +15,7 @@ use Statikbe\FilamentFlexibleContentBlocks\Models\Contracts\Linkable;
 class CallToActionData
 {
     public function __construct(
-        public string $url,
+        public ?string $url,
         public ?string $label = null,
         public ?string $buttonStyle = null,
         public bool $openNewWindow = false,
@@ -33,6 +35,7 @@ class CallToActionData
             throw CallToActionNotDefinedException::create('The call to action data does not specify a route, url or model.');
         }
 
+        $url = null;
         if ($callToActionBlockData[CallToActionField::FIELD_CTA_MODEL] === CallToActionType::TYPE_URL) {
             $url = $callToActionBlockData[CallToActionField::FIELD_URL];
         } elseif ($callToActionBlockData[CallToActionField::FIELD_CTA_MODEL] === CallToActionType::TYPE_ROUTE) {
@@ -46,9 +49,15 @@ class CallToActionData
                 throw new LinkableModelNotFoundException("No linkable model could be found for '{$linkableType}' in morph map.");
             }
 
-            /** @var Linkable&Model $page */
-            $page = $linkableModel::findOrFail($callToActionBlockData[CallToActionField::FIELD_ENTRY_ID]);
-            $url = $page->getViewUrl();
+            try {
+                /** @var Linkable&Model $page */
+                $page = $linkableModel::findOrFail($callToActionBlockData[CallToActionField::FIELD_ENTRY_ID]);
+                $url = $page->getViewUrl();
+            }
+            catch(ModelNotFoundException $ex){
+                //The url could not be created because the entry could not be found. By catching the exception, we avoid a 404.
+                Log::warning("The url could not be created because the entry could not be found ({$linkableModel}: {$callToActionBlockData[CallToActionField::FIELD_ENTRY_ID]}). This will probably result in a dead link.");
+            }
         }
 
         $buttonStyle = $callToActionBlockData[CallToActionField::FIELD_BUTTON_STYLE] ?? null;
