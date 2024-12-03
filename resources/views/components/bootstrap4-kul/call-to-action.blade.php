9f24ff6ac869df7e837366aec0562fbec877ab0a
sevbesau <seppe@statik.be>
2024-12-03 15:04:42 +0100

    resolve comment -> wrap cta in if url block

--- resources/views/components/bootstrap4-kul/call-to-action.blade.php
+++ resources/views/components/bootstrap4-kul/call-to-action.blade.php
@@ -3,22 +3,24 @@
 @endphp
 
 
-<a 
-    @class([
-        'btn', 
-        isset($callToAction->buttonStyle) => $callToAction->buttonStyle
-    ])
-    href="{{ $callToAction->url }}"
-    @if (isset($callToAction->label))
-        title="{{ Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks::replaceParameters($callToAction->label) }}" 
-    @endif
-    @if (isset($callToAction->openNewWindow)) 
-        target="_blank" rel="noopener noreferrer" 
-    @endif
->
-    @if (isset($callToAction->label))
-        {{ Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks::replaceParameters($callToAction->label) }}
-    @else
-        &xrarr;
-    @endif
-</a>
+@if($callToAction->url)
+    <a
+        @class([
+            'btn',
+            isset($callToAction->buttonStyle) => $callToAction->buttonStyle
+        ])
+        href="{{ $callToAction->url }}"
+        @if (isset($callToAction->label))
+            title="{{ Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks::replaceParameters($callToAction->label) }}"
+        @endif
+        @if (isset($callToAction->openNewWindow))
+            target="_blank" rel="noopener noreferrer"
+        @endif
+    >
+        @if (isset($callToAction->label))
+            {{ Statikbe\FilamentFlexibleContentBlocks\FilamentFlexibleContentBlocks::replaceParameters($callToAction->label) }}
+        @else
+            &xrarr;
+        @endif
+    </a>
+@endif
