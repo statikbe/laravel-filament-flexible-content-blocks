a88fe54156f5e19f20b591dd050f8abd148344ea
Ben Verbist <ben@statik.be>
2025-09-30 15:12:51 +0200

    [flex content blocks] "collapsible group" content block : small changes after feedback

--- src/ContentBlocks/CollapsibleGroupBlock.php
+++ src/ContentBlocks/CollapsibleGroupBlock.php
@@ -50,23 +50,19 @@ class CollapsibleGroupBlock extends AbstractFilamentFlexibleContentBlock
     {
         parent::__construct($record, $blockData);
 
-        $this->groupTitle = $blockData[self::GROUP_TITLE_FIELD] ?? null;
-        $this->groupIntro = $blockData[self::GROUP_INTRO_FIELD] ?? null;
-        $this->backgroundColourType = $blockData[self::BACKGROUND_COLOUR_FIELD] ?? null;
-        $this->collapsibleItems = $this->mapToCollapsibleItemsData($blockData[self::COLLAPSIBLE_ITEMS_FIELD]);
+        $this->groupTitle = $blockData[static::GROUP_TITLE_FIELD] ?? null;
+        $this->groupIntro = $blockData[static::GROUP_INTRO_FIELD] ?? null;
+        $this->backgroundColourType = $blockData[static::BACKGROUND_COLOUR_FIELD] ?? null;
+        $this->collapsibleItems = $this->mapToCollapsibleItemsData($blockData[static::COLLAPSIBLE_ITEMS_FIELD]);
     }
 
     private function mapToCollapsibleItemsData(array $items): Collection
     {
-        if (empty($items)) {
-            return collect([]);
-        }
-
         return collect($items)->map(function (array $item) {
             return new CollapsibleItemData(
-                title: $item[self::ITEM_TITLE_FIELD],
-                text: $item[self::ITEM_TEXT_FIELD],
-                isOpenByDefault: $item[self::ITEM_IS_OPEN_BY_DEFAULT_FIELD] ?? false,
+                title: $item[static::ITEM_TITLE_FIELD],
+                text: $item[static::ITEM_TEXT_FIELD],
+                isOpenByDefault: $item[static::ITEM_IS_OPEN_BY_DEFAULT_FIELD] ?? false,
             );
         });
     }
@@ -78,7 +74,7 @@ class CollapsibleGroupBlock extends AbstractFilamentFlexibleContentBlock
 
     public static function getContentSummary(array $state): ?string
     {
-        return $state[self::GROUP_TITLE_FIELD] ?? $state[self::GROUP_INTRO_FIELD];
+        return $state[static::GROUP_TITLE_FIELD] ?? $state[static::GROUP_INTRO_FIELD];
     }
 
     public static function getIcon(): string
@@ -89,30 +85,30 @@ class CollapsibleGroupBlock extends AbstractFilamentFlexibleContentBlock
     protected static function makeFilamentSchema(): array|Closure
     {
         return [
-            TextInput::make(self::GROUP_TITLE_FIELD)
+            TextInput::make(static::GROUP_TITLE_FIELD)
                 ->label(static::getFieldLabel('group_title'))
                 ->maxLength(255),
-            RichEditor::make(self::GROUP_INTRO_FIELD)
+            RichEditor::make(static::GROUP_INTRO_FIELD)
                 ->label(static::getFieldLabel('group_intro'))
                 ->toolbarButtons(static::getIntroToolbarButtons()),
             BackgroundColourField::create(static::class),
-            Repeater::make(self::COLLAPSIBLE_ITEMS_FIELD)
+            Repeater::make(static::COLLAPSIBLE_ITEMS_FIELD)
                 ->label(static::getFieldLabel('collapsible_items'))
                 ->schema([
-                    TextInput::make(self::ITEM_TITLE_FIELD)
+                    TextInput::make(static::ITEM_TITLE_FIELD)
                         ->label(static::getFieldLabel('item_title'))
                         ->required()
                         ->maxLength(255),
-                    RichEditor::make(self::ITEM_TEXT_FIELD)
+                    RichEditor::make(static::ITEM_TEXT_FIELD)
                         ->label(static::getFieldLabel('item_text'))
                         ->toolbarButtons(static::getItemTextToolbarButtons())
                         ->required(),
-                    Toggle::make(self::ITEM_IS_OPEN_BY_DEFAULT_FIELD)
+                    Toggle::make(static::ITEM_IS_OPEN_BY_DEFAULT_FIELD)
                         ->label(static::getFieldLabel('item_is_open_by_default'))
                         ->default(false),
                 ])
                 ->itemLabel(function (array $state): ?string {
-                    return $state[self::ITEM_TITLE_FIELD];
+                    return $state[static::ITEM_TITLE_FIELD];
                 })
                 ->collapsible()
                 ->minItems(1),
