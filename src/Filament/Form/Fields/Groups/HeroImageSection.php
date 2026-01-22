feb1be43c77750d9c44938953cc2eb4eb402b333
Sten Govaerts <sten.govaerts@gmail.com>
2026-01-22 16:45:00 +0100

    make video url field in hero image section optional with create flag

--- src/Filament/Form/Fields/Groups/HeroImageSection.php
+++ src/Filament/Form/Fields/Groups/HeroImageSection.php
@@ -11,20 +11,23 @@ use Statikbe\FilamentFlexibleContentBlocks\Filament\Form\Fields\HeroVideoUrlFiel
 
 class HeroImageSection extends Section
 {
-    public static function create(bool $translatableImage = false): static
+    public static function create(bool $translatableImage = false, bool $enableVideoUrlField=false): static
     {
+        $fields = [
+            HeroImageField::create($translatableImage),
+            Grid::make(1)->schema([
+                HeroImageTitleField::create(),
+                HeroImageCopyrightField::create(),
+            ])->columnSpan(1),
+        ];
+
+        if($enableVideoUrlField) {
+            $fields[] = HeroVideoUrlField::create();
+        }
+
         return static::make(trans('filament-flexible-content-blocks::filament-flexible-content-blocks.form_component.hero_image_section_title'))
             ->schema([
-                Grid::make(2)->schema([
-                    HeroImageField::create($translatableImage),
-
-                    Grid::make(1)->schema([
-                        HeroImageTitleField::create(),
-                        HeroImageCopyrightField::create(),
-                    ])->columnSpan(1),
-
-                    HeroVideoUrlField::create(),
-                ]),
+                Grid::make(2)->schema($fields),
             ]);
     }
 }
