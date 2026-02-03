1d6805672155f306cf65a318d8a20c9561a22e57
lukasdewijn <dewijngaertl@gmail.com>
2026-02-03 16:52:23 +0100

    moved the ViewAction out of the block-pages

---
+++ src/Filament/Table/Actions/ViewPageAction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Statikbe\FilamentFlexibleContentBlocks\Filament\Table\Actions;
+
+use Filament\Actions\Action;
+use Filament\Resources\Pages\Page as FilamentPage;
+use Statikbe\FilamentFlexibleContentBlocks\Models\Contracts\Linkable;
+
+class ViewPageAction extends Action
+{
+    public static function getDefaultName(): ?string
+    {
+        return 'view_page';
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this
+            ->label(trans('filament-flexible-content-blocks::filament-flexible-content-blocks.table_action.view_page_lbl'))
+            ->color('gray')
+            ->icon('heroicon-s-eye')
+            ->url(function (Linkable $record, FilamentPage $livewire): string {
+                $locale = $livewire->getActiveFormsLocale() ?? app()->getLocale();
+
+                return $record->getPreviewUrl($locale);
+            })
+            ->openUrlInNewTab();
+    }
+}
