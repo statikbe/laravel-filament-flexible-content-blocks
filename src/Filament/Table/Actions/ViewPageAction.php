e4842cdc5b825f4a169aadfdaeeb71fd5d4352bf
lukasdewijn <dewijngaertl@gmail.com>
2026-02-03 13:48:22 +0100

    Made a ViewPageAction.php that can be used to bring the user to the relevant page.

---
+++ src/Filament/Table/Actions/ViewPageAction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Statikbe\FilamentFlexibleContentBlocks\Filament\Table\Actions;
+
+use Filament\Actions\Action;
+
+use Filament\Resources\Pages\Page as FilamentPage;
+use Statikbe\FilamentFlexibleContentBlocks\Models\Contracts\Linkable;
+
+class ViewPageAction extends Action
+{
+    public static function getDefaultName(): ?string
+    {
+        return 'view_page';
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this
+            ->label(trans('filament-flexible-content-blocks::filament-flexible-content-blocks.table_action.view_page_lbl'))
+            ->color('gray')
+            ->icon('heroicon-s-eye')
+            ->url(function (Linkable $record, FilamentPage $livewire): string {
+                $locale = app()->getLocale();
+
+                if (method_exists($livewire, 'getActiveLocale')) {
+                    $locale = $livewire->getActiveLocale();
+                }
+
+                return $record->getPreviewUrl($locale);
+            })
+            ->openUrlInNewTab();
+    }
+}
